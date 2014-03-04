9ac2cf9dfa4fceb13c132dc9df4bb1ba4836cb0d
matjaz <matjaz@10.0.0.7>
2014-03-03 23:12:10 -0800

    Aligned company name in header to the left.

--- GoldenHouse/application/views/Portal_header.php
+++ GoldenHouse/application/views/Portal_header.php
@@ -2,16 +2,15 @@
 
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/bootstrap.css' ?>" />
+<link rel="stylesheet" type="text/css"
+	href="<?php echo base_url().'css/bootstrap.css' ?>" />
 </head>
 <body>
 	<div class="container">
 		<div class="row">
 			<div class="span12">
 				<div class="well">
-					<center>
-						<h1>GoldenHouse LLC</h1>
-					</center>
+					<h1 align="left">GoldenHouse LLC</h1>
 				</div>
 			</div>
 		</div>
