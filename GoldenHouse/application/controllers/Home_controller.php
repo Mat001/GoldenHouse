f0fc2d932a77c035b4b595066cfb6b6c6562d7f2
Mat001 <matjaz@matjazpirnovar.com>
2014-03-05 00:06:44 -0800

    Applied Codeigniters (simple) unit testing after a day of trying to set up PHPUnit with Codeigniter (just didn't want to work grrr).

--- GoldenHouse/application/controllers/Home_controller.php
+++ GoldenHouse/application/controllers/Home_controller.php
@@ -1,11 +1,57 @@
 <?php
 
-class home_controller extends CI_Controller
+class Home_controller extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('unit_test');
+	}
+	
+	
+	
 	public function index()
 	{
 		$this->load->view('Portal_home');
 	}
+	
+	public function testHome_controller()
+	{
+		$this->unit->run(add(3, 4), 7, "testing_index_function"); // @TODO
+		$this->unit->run(multiply(2, 4), 8, "testing_multiply_function");
+		$this->unit->run(subtract(9, 5),4, "testing_subtract_function");
+		
+		echo $this->unit->report();
+		
+	}
+	
 }
+
+
+
+/**
+ * Test funtions for Home_controller
+ * @param unknown $a
+ * @param unknown $b
+ * @return number
+ */
+
+function add($a, $b)
+{
+	return $a + $b;
+}
+
+function multiply($c, $d)
+{
+	return $c * $d;
+}
+
+function subtract($e, $f)
+{
+	return $e - $f;
+}
+
+
+
 
 ?>
--- GoldenHouse/system/core/Utf8.php
+++ GoldenHouse/system/core/Utf8.php
@@ -38,7 +38,7 @@ class CI_Utf8 {
 	{
 		log_message('debug', "Utf8 Class Initialized");
 
-		global $CFG;
+		$CFG =& load_class('Config', 'core');			// global $CFG;  -- used to be this. matjaz changed: http://www.jamesfairhurst.co.uk/posts/view/codeigniter_phpunit_and_netbeans
 
 		if (
 			preg_match('/./u', 'é') === 1					// PCRE must support UTF-8
